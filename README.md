286acef866335ce7c8d822d92afe2d43b4a78e7c
Harald Kirsch <pifpafpuf@gmx.de>
2019-01-20 16:38:48 +0100

    initial commit, works for trivial tests

---
+++ README.md
@@ -0,0 +1,4 @@
+# Quick-Starting Solr
+Solr comes with a guide to run it from the unpacked .tar.gz or installation directory. But the data then ends up in the install directory. The little scripts here help to avoid this and try to keep the installation directory as pristine as possible.
+
+
---
+++ helpers.sh
@@ -0,0 +1,35 @@
+## sourced in from other scripts, contains helper functions
+initSolrFile() {
+  local path="$1"
+  local file=$(basename "$path")
+  if [ -e "$SQSDATA/config/$file" ]; then
+    return
+  fi
+
+  local solr=$(type -p solr)
+  if [ -z "$solr" ]; then
+    echo solr command must available via PATH
+    exit 1
+  fi
+  local solrDir=$(dirname $(dirname $(readlink -f "$solr")))
+  mkdir -p "$SQSDATA/config"
+  cp "$solrDir/$path" "$SQSDATA/config"
+}
+
+isEmpty() {
+set -x
+  local varname="$1"
+  local varvalue="${!varname}"
+  if [ -z "$varvalue" ]; then
+    shift
+    echo >&2 $varname "$@"
+    exit 1
+  fi
+}
+getZk() {
+  if [ "$ZKROOT" = "_" ]; then
+    echo "localhost:9983/solr"
+  else
+    echo "$ZKROOT"
+  fi
+}
